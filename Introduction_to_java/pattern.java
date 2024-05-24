6f6aec486c60ea5cf69778c2e9c887b2f480ae2c
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2024-05-24 17:19:56 +0530

    Add files via upload

---
+++ Introduction_to_java/pattern.java
@@ -0,0 +1,9 @@
+public class pattern {
+    public static void main(String arg[]) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+    
+}
