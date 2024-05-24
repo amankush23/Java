22dff251e2b177ad2f73d2182725c1502f7be9f6
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2024-05-24 17:33:03 +0530

    Add files via upload

---
+++ Introduction_to_java/variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String arg[]){
+        int a = 12;
+        int b = 23;
+        int sum = a + b;
+        System.out.println(sum);   
+     }
+}
