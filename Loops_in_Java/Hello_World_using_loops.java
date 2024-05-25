723e998fb7e88cfc9c508462bf58c40faefbe161
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2024-05-25 11:57:32 +0530

    Add files via upload

---
+++ Loops_in_Java/Hello_World_using_loops.java
@@ -0,0 +1,8 @@
+class Hello_World_using_loops{
+    public static void main(String[] args){
+        // for(initialization; condition; updation){Statement that is to be executed!}
+        for(int counter = 0; counter < 3; counter = counter + 1){
+            System.out.println("Hello World!");
+        }
+    }
+}
