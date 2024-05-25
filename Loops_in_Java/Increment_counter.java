15c741ccf5d4906dcef70ff93a0a7a6d0dc78377
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2024-05-25 12:12:33 +0530

    Add files via upload

---
+++ Loops_in_Java/Increment_counter.java
@@ -0,0 +1,8 @@
+public class Increment_counter {
+    public static void main(String[] args){
+        //i++ => i = i + 1
+        for(int i = 0; i < 11; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
