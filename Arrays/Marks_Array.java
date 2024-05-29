616d2822fc5172d05e73c64da7535b5271ae8afe
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2024-05-29 20:28:32 +0530

    Add files via upload

---
+++ Arrays/Marks_Array.java
@@ -0,0 +1,12 @@
+class Marks_Array{
+    public static void main(String[] args){
+        int[] marks = new int[3];
+        marks[0] = 97; //Physics
+        marks[1] = 99; //English
+        marks[2] = 87; //Hindi
+        for(int i = 0; i <= 3; i++){
+            System.out.println(marks[i]);
+        }
+        
+    }
+}
