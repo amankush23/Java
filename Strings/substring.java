473799da54d506d65d8158ac9f59ffc7a3457299
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2024-06-02 22:17:12 +0530

    Add files via upload

---
+++ Strings/substring.java
@@ -0,0 +1,6 @@
+public class substring {
+    public static void main(String[] args) {
+        String Sentence = "Hello guys welcome to my first program of Substring!";
+        System.out.print(Sentence.substring(6, Sentence.length()));
+    }
+}
