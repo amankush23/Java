d9981e46bed73d08f6a79f3f020590cf252b4ef1
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-02-06 12:19:37 +0530

    Add files via upload

---
+++ CodeForces Question/catch_the_coin.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class catch_the_coin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+    while(n>0){
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(y<=-2){
+            System.out.println("No");
+        }else{
+            System.out.println("Yes");
+        }
+        n--;
+        
+ 
+        }
+    }
+}
