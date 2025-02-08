7a21ce8a531116307e8eb4b10bcde84835b6eba1
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-02-08 23:29:09 +0530

    Add files via upload

---
+++ Strings/Static_Variable.java
@@ -0,0 +1,14 @@
+public class Static_Variable {
+    static int val = 100;
+    public static void main(String[] args) {
+        System.out.println(val);
+        System.out.println(Add(2,7));
+        System.out.println(val);
+    }
+    public static int Add(int a, int b){
+        int val = 90;
+        Static_Variable.val = Static_Variable.val+5;
+        return a+b;
+    }
+
+}
