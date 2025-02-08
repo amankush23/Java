51ff16a66f9759b817a2f33c33da7267929a547d
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-02-08 23:28:47 +0530

    Add files via upload

---
+++ Strings/Playing_with_Good_String.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Playing_with_Good_String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.print(goodString(s));
+    }
+    public static int goodString(String s){
+        int ans = 0; 
+        int c = 0;
+        for(int i = 0 ; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(isvowels(ch)){
+                c++;
+            }
+            else{
+                ans = Math.max(ans,c);
+                c=0;
+            }
+        }
+        ans = Math.max(ans, c);
+        return ans;
+    }
+    public static boolean isvowels(char ch){
+        if(ch =='a' || ch == 'e' || ch =='i' || ch == 'o' || ch == 'u'){
+            return true; 
+        }
+        return false;
+         }
+    }
+
