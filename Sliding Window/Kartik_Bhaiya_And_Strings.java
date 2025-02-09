16bb6d44eb2e871b0b49628d414f5611dd4d55ee
AMAN KUSHWAH <142593058+amankush23@users.noreply.github.com>
2025-02-09 09:35:08 +0530

    Add files via upload

---
+++ Sliding Window/Kartik_Bhaiya_And_Strings.java
@@ -0,0 +1,29 @@
+package Basic_Sliding_Window;
+import java.util.*;
+public class Kartik_Bhaiya_And_Strings {
+    public static void main(String[] args) {
+        String s = "ababbabbaaaaababab";
+        int k = 2;
+        int flip_a = Maxlen(s, 'a', k);
+        int flip_b = Maxlen(s, 'b', k);
+        System.out.println(Math.max(flip_a, flip_b));
+
+    }
+    public static int Maxlen(String s, char ch , int k){
+        int si = 0 ,ei = 0, flip = 0, ans = 0; 
+        while(ei < s.length()){
+            if(s.charAT(ei) == ch){
+                flip++;
+            } 
+            while(flip > k ){
+                if(s.charAt(si) == ch){
+                    flip--;
+                }
+                si++;
+            }
+            ans = Math.max(ans, ei-si+1);
+            ei++;
+        }
+        return ans;
+    }
+}
